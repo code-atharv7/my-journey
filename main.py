122db9e37f42389ed1f7e918658d9ad942f30a50
code-atharv7 <atharvsharmalive@gmail.com>
2024-12-26 12:22:48 +0530

    Twinkle Khanna

---
+++ main.py
@@ -0,0 +1,14 @@
+print('''Twinkle, twinkle, little star,
+How I wonder what you are,
+Up above the world so high,
+Like a diamond in the sky.
+
+When the traveler in the dark
+Thanks you for thy tiny spark,
+He could not see which way to go,
+If you did not twinkle so.
+
+When the blazing sun is gone,
+And he nothing shines upon,
+Then appears thy tiny spark,
+Twinkle, twinkle, in the dark.''')
